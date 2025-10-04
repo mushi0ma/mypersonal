d11f2d50ac5fde02f15926b0f8fc3475064a1f5d
mushi0ma <contact.mushi0ma@gmail.com>
2025-10-04 22:19:33 +0500

    fixing bugs with testers

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,18 +6,8 @@ from psycopg2 import pool
 from dotenv import load_dotenv
 import re
 
-# --- ВРЕМЕННОЕ РЕШЕНИЕ: Жестко задаем данные для подключения ---
-TEST_DB_CONFIG = {
-    "dbname": "test_db",
-    "user": "test_user",
-    "password": "test_password",
-    "host": "localhost",
-    "port": "5434" # Убедитесь, что порт верный
-}
-# -----------------------------------------------------------
-
-# Загружаем остальные переменные (не для БД)
-load_dotenv(dotenv_path='.env.test', encoding='utf-8')
+# Загружаем переменные из .env.test
+load_dotenv(dotenv_path='.env.test')
 
 @pytest.fixture(scope="session")
 def test_db_connection():
@@ -25,8 +15,15 @@ def test_db_connection():
     conn_pool = None
     conn = None
     try:
-        # Используем наши жестко заданные данные вместо os.getenv()
-        conn_pool = psycopg2.pool.SimpleConnectionPool(minconn=1, maxconn=1, **TEST_DB_CONFIG)
+        # Используем те же переменные, что и docker-compose.test.yml
+        conn_pool = psycopg2.pool.SimpleConnectionPool(
+            minconn=1, maxconn=1,
+            dbname=os.getenv("POSTGRES_DB"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
         conn = conn_pool.getconn()
         print("\n--- Connected to TEST database ---")
         yield conn
@@ -41,15 +38,15 @@ def test_db_connection():
 def db_session(test_db_connection):
     """Для каждого теста: создает и очищает таблицы."""
     cursor = test_db_connection.cursor()
-    
+
     with open('init_db.py', 'r', encoding='utf-8') as f:
         content = f.read()
-    
+
     create_table_commands = re.findall(r'"""\s*(CREATE TABLE.*?);', content, re.DOTALL)
-    
+
     for command in create_table_commands:
         cursor.execute(command)
-    
+
     test_db_connection.commit()
     print("\n--- Tables created ---")
 
